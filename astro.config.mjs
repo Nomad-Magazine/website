8e681502935a187cb890fc9d10980d998a6e2ad0
Rishi <jain71000@gmail.com>
2025-06-30 13:11:43 +0530

    ..>

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,16 +6,12 @@ import cloudflare from '@astrojs/cloudflare'
 import { defineConfig, envField } from 'astro/config'
 
 export default defineConfig({
-  adapter: cloudflare(),
-  integrations: [sitemap()],
-  site: 'https://nomad-magazine-main.pitiakova.workers.dev',
-  env: {
-    schema: {
-      SMART_SUITE_APIKEY: envField.string({ context: 'server', access: 'secret' }),
-      SMART_SUITE_ACCOUNT_ID: envField.string({ context: 'server', access: 'secret' }),
-      SMART_SUITE_TABLE_ID_COMPANY: envField.string({ context: 'server', access: 'secret' }),
-    },
+  server: {
+    open: true,
+    port: 3000,
+    host: '0.0.0.0',
   },
+  adapter: cloudflare(),
   vite: {
     plugins: [
       tailwindcss(),
@@ -24,9 +20,13 @@ export default defineConfig({
       }),
     ],
   },
-  server: {
-    open: true,
-    port: 3000,
-    host: '0.0.0.0',
+  integrations: [sitemap()],
+  site: 'https://nomad-magazine-main.pitiakova.workers.dev',
+  env: {
+    schema: {
+      SMART_SUITE_APIKEY: envField.string({ context: 'server', access: 'secret' }),
+      SMART_SUITE_ACCOUNT_ID: envField.string({ context: 'server', access: 'secret' }),
+      SMART_SUITE_TABLE_ID_COMPANY: envField.string({ context: 'server', access: 'secret' }),
+    },
   },
 })
