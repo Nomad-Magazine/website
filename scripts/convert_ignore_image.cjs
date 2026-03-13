ee4b3524fe09c0a203b59dcdb75d06e284ea085f
Martin Donadieu <martindonadieu@gmail.com>
2026-03-13 12:34:34 +0100

    fix: missing capchat

--- scripts/convert_ignore_image.cjs
+++ scripts/convert_ignore_image.cjs
@@ -1,6 +1,5 @@
 const sharp = require('sharp');
 const fs = require('fs');
-const path = require('path');
 
 const srcPath = '/Users/pityakova/.cursor/projects/Users-pityakova-Documents-NGM-Website/assets/nomad-cafe-ignore-collage.png';
 const destPath = '/Users/pityakova/Documents/NGM/Website/public/images/nomad-cafe-ignore-collage.webp';
--- scripts/convert_user_image.cjs
+++ scripts/convert_user_image.cjs
@@ -1,6 +1,5 @@
 const sharp = require('sharp');
 const fs = require('fs');
-const path = require('path');
 
 const srcPath = '/Users/pityakova/.cursor/projects/Users-pityakova-Documents-NGM-Website/assets/CleanShot_2026-03-12_at_14.42.10-ccd9590c-02ad-4829-ab7f-fb210b89c305.png';
 const destPath = '/Users/pityakova/Documents/NGM/Website/public/images/nomad-connection-best-collage.webp';
